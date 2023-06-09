aaf3e0cc9f8a047587674c8b09837f8311254986
vincentpham123 <vincentpham32@gmail.com>
2023-06-09 14:33:50 -0700

    added hovers

--- practice-for-sprint-15-react-redux-pet-rating-main/frontend/src/components/PawsRatingInput.js
+++ practice-for-sprint-15-react-redux-pet-rating-main/frontend/src/components/PawsRatingInput.js
@@ -1,12 +1,47 @@
+import {useState} from 'react';
 const PawsRatingInput = ({ rating, disabled, onChange }) => {
-
+const [activeRating,setActiveRating] = useState(rating);
+const mouseHoverEnterHandler = (event,number) => {
+  if(disabled===false){
+    setActiveRating(number)
+  }
+}
+const mouseHoverLeaveHandler = (e) => {
+  if(disabled===false){
+    setActiveRating(activeRating)
+  }
+}
   return (
+    <>
     <input
       type="number"
       disabled={disabled}
       value={rating}
       onChange={onChange}
     />
+     <div className='rating-input'>
+        <div className={activeRating>0 ? 'filled' : 'empty'} onMouseEnter={(event)=>mouseHoverEnterHandler(event,1)} onMouseLeave={(event)=>mouseHoverLeaveHandler(event)}
+        >
+            <i className='fa fa-paw'></i>
+        </div>
+        <div className={activeRating>1 ? 'filled' : 'empty'} onMouseEnter={(event)=>mouseHoverEnterHandler(event,2)} onMouseLeave={(event)=>mouseHoverLeaveHandler(event)
+        }>
+            <i className='fa fa-paw'></i>
+        </div>
+        <div className={activeRating>2 ? 'filled' : 'empty'} onMouseEnter={(event)=>mouseHoverEnterHandler(event,3)
+        } onMouseLeave={(event)=>mouseHoverLeaveHandler(event)}>
+            <i className='fa fa-paw'></i>
+        </div>
+        <div className={activeRating>3 ? 'filled' : 'empty'} onMouseEnter={(event)=>mouseHoverEnterHandler(event,4)
+        } onMouseLeave={(event)=>mouseHoverLeaveHandler(event)}>
+            <i className='fa fa-paw'></i>
+        </div>
+        <div className={activeRating>4 ? 'filled' : 'empty'} onMouseEnter={(event)=>mouseHoverEnterHandler(event,5)
+        } onMouseLeave={(event)=>mouseHoverLeaveHandler(event)}>
+            <i className='fa fa-paw'></i>
+        </div>
+    </div> 
+    </>
   );
 };
 
